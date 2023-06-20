5bce8ec98cfcb7108a3ebe055704e8eefe2a820e
Adanato <66979547+Adanato@users.noreply.github.com>
2023-06-20 00:50:53 -0400

    Following the tutorial. Adding more components and fixing flow

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,39 +1,42 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import MainHeader from "./MainHeader";
-import NavLinks from "./NavLinks";
-import SideDrawer from "./SideDrawer";
-import Backdrop from "../UIElements/Backdrop";
-import "./MainNavigation.css";
+import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
+import './MainNavigation.css';
 
-const MainNavigation = (props) => {
+const MainNavigation = props => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  
-  
-  const closeDrawerHandler = () => {
-    setDrawerIsOpen(false);
-  };
+
   const openDrawerHandler = () => {
     setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
   };
 
   return (
     <React.Fragment>
       {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
-      <SideDrawer onClick={closeDrawerHandler} show={drawerIsOpen} >
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
-          <NavLinks/>
+          <NavLinks />
         </nav>
       </SideDrawer>
 
-      <MainHeader >
-        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
         </button>
-        <h1 className="main-navigation__title" >
+        <h1 className="main-navigation__title">
           <Link to="/">YourPlaces</Link>
         </h1>
         <nav className="main-navigation__header-nav">
